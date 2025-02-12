4127816f0e8f79da22d29f9d5eb924b424efc12c
reign <reignoffroze@hotmail.com>
2025-02-12 14:33:30 -0500

    Fix spacing

--- bacvat.lua
+++ bacvat.lua
@@ -5,23 +5,23 @@ local hatches = {}
 
 print("Discovering hatches ...")
 for address, name in component.list("transposer", true) do
-  local proxy = component.proxy(address)
-  print("Found transposer @ " .. address)
-  for _, side in ipairs({0, 1, 2, 3, 4, 5}) do
-    if proxy.getInventoryName(side) == "gt.blockmachines" then
-      local capacity = proxy.getTankCapacity(side)
-      if capacity ~= 0 then
-        print("Found hatch on side " .. sides[side] .. " with capacity " .. capacity)
-        table.insert(hatches, {proxy, side, capacity / 2})
-      end
+    local proxy = component.proxy(address)
+    print("Found transposer @ " .. address)
+    for _, side in ipairs({ 0, 1, 2, 3, 4, 5 }) do
+        if proxy.getInventoryName(side) == "gt.blockmachines" then
+            local capacity = proxy.getTankCapacity(side)
+            if capacity ~= 0 then
+                print("Found hatch on side " .. sides[side] .. " with capacity " .. capacity)
+                table.insert(hatches, { proxy, side, capacity / 2 })
+            end
+        end
     end
-  end
 end
 
 function rebalance()
     for _, hatch in ipairs(hatches) do
         local proxy, side, half = table.unpack(hatch)
-        local potentialSinkSides = {0,2,3,4,5}
+        local potentialSinkSides = { 0, 2, 3, 4, 5 }
         local sinkSide = nil
         local level = proxy.getFluidInTank(side)[1].amount
         if level > half then
@@ -32,7 +32,10 @@ function rebalance()
                 end
             end
             if sinkSide ~= nil then
-                print("Transferring " .. level - half .. "L of fluid from tank on side " .. side .. " to tank on side " .. sinkSide .. " of transposer " .. proxy.address)
+                print("Transferring " ..
+                level - half ..
+                "L of fluid from tank on side " ..
+                side .. " to tank on side " .. sinkSide .. " of transposer " .. proxy.address)
                 proxy.transferFluid(side, sinkSide, level - half)
             end
         end
@@ -43,8 +46,8 @@ rebalance()
 while true do
     local id, _, _, _ = event.pullMultiple("redstone_changed", "interrupted")
     if id == "interrupted" then
-      print("Interrupt received, exiting...")
-      break
+        print("Interrupt received, exiting...")
+        break
     end
     rebalance()
 end
